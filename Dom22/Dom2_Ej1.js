dac1d7db3851c960a524a6f608e4f83e03736e29
hgsgithub <62034695+hgsgithub@users.noreply.github.com>
2022-06-11 12:20:14 +0200

    Para el 18_06_V1

--- Dom22/Dom2_Ej1.js
+++ Dom22/Dom2_Ej1.js
@@ -1,1 +1,9 @@
+//console.log(document.querySelector("#div1 a").title)//imprime hola mundo
+//console.log(document.querySelector("#div1 a")) 
+//console.log(document.querySelector("#div1 >p").textContent)
+//var titulo=document.querySelectorAll("#div1 [title]")
+//console.log(titulo[2].text); //Hola de Nuevo
+//console.log(document.querySelector("#div1 >p >a").title)
+console.log(document.querySelector(".normalLink[title^='adios']").title);
+
 
